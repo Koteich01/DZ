0ffc89ffa3ee58411940ee27196a4738cfacef78
Sergey <SergeyNesterov01@gmail.com>
2022-08-08 18:05:12 +0500

    asd

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите два числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
--- task1/Program.cs
+++ task1/Program.cs
@@ -1,6 +1,6 @@
-﻿console.Write("Введите целое число: ");
-int a = Convert.ToInt32(System.console.ReadLine());
-console.Write("Введите еще одно целое число: ");
-int b = Convert.ToInt32(System.console.ReadLine());
-if (b*b == a) console.WriteLine($"число {a} является квадратом числа {b}");
-else console.WriteLine($"число {a} не является квадратом числа {b}");
+﻿Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(System.Console.ReadLine());
+Console.Write("Введите еще одно целое число: ");
+int b = Convert.ToInt32(System.Console.ReadLine());
+if (b*b == a) Console.WriteLine($"число {a} является квадратом числа {b}");
+else Console.WriteLine($"число {a} не является квадратом числа {b}");
