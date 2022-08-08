0885d12614cc0275ae3ca6abe0ba2d70e247886c
Sergey <SergeyNesterov01@gmail.com>
2022-08-08 17:00:29 +0500

    asd

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿console.Write("Введите целое число: ");
+int a = Convert.ToInt32(System.console.ReadLine());
+console.Write("Введите еще одно целое число: ");
+int b = Convert.ToInt32(System.console.ReadLine());
+if (b*b == a) console.WriteLine($"число {a} является квадратом числа {b}");
+else console.WriteLine($"число {a} не является квадратом числа {b}");
